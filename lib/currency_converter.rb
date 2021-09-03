836d50970f7a76976e4dda428a35d76a000ca4e0
Rizwan Ali <rizwanali1055@gmail.com>
2021-09-03 18:18:28 +0500

    first commit

---
+++ lib/currency_converter.rb
@@ -0,0 +1,6 @@
+require "currency_converter/version"
+
+module CurrencyConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/currency_converter_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CurrencyConverter do
+  it "has a version number" do
+    expect(CurrencyConverter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
